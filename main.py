b499c1bff027aa2fd5389619d1247678a8fe0734
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-08-05 09:58:49 +1000

    Set date as the to-do list header

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+"""
+TO DO:
+1. Create an empty database.
+2. Create a text field where the user can type in a task.
+3. Enter the text into the database.
+4. If the user crosses out an item, present the task as crossed out.
+5. If all tasks are done, show a funny gif.
+6. Find way to edit the title of the list when the title is pressed.
+"""
+
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,7 +22,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", date=datetime.today().strftime("%d/%m"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
