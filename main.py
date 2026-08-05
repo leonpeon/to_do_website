d9885a71c8d38e5dae4592cde3a17cad1616a2ce
leonpeon <59907683+leonpeon@users.noreply.github.com>
2026-08-05 13:28:08 +1000

    Add delete functionality
    Add congratulatory image if you complete all your tasks

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ TO DO:
 """
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
@@ -32,11 +32,8 @@ with app.app_context():
     db.create_all()
 
 # Deletes old tasks
-db.session.execute(db.delete(Todo).where(Todo.date_created != date.today()))
-db.session.commit()
-
-# Gets list of task from the database
-task_list = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars().all()
+    db.session.execute(db.delete(Todo).where(Todo.date_created != date.today()))
+    db.session.commit()
 
 # PAGES
 @app.route("/", methods=["GET", "POST"])
@@ -49,10 +46,34 @@ def home():
         )
         db.session.add(new_todo)
         db.session.commit()
-        
-    return render_template("index.html", date=date.today().strftime("%d/%m"), tasks=task_list)
 
+    # Gets list of task from the database
+    task_list = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars().all()
 
+    # If all tasks are done, show a gif
+    show_gif = False
+    if len(task_list) > 0:
+        task_done = []
+        for task in task_list:
+            task_done.append(task.complete)
+        if 0 not in task_done:  
+            show_gif = True
+    
+    return render_template("index.html", date=date.today().strftime("%d/%m"), tasks=task_list, show_gif=show_gif)
+
+@app.route('/cross/<int:post_id>', methods=["GET", "POST"])
+def complete_task(post_id):
+    requested_task = db.get_or_404(Todo, post_id)
+    requested_task.complete = True
+    db.session.commit()
+    return redirect("/")
+
+@app.route('/delete/<int:post_id>', methods=["GET", "POST"])
+def delete_task(post_id):
+    requested_task = db.get_or_404(Todo, post_id)
+    db.session.delete(requested_task)
+    db.session.commit()
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
